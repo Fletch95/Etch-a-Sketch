0f94ece45466ea38bf4c04332e85d8642cdac789
Thomas Fletcher <t_fletcher@hotmail.co.uk>
2022-08-20 00:34:11 +0100

    Add hover fill functionality with reset button

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const container = document.querySelector('.container');
+const resetButton = document.querySelector('.reset');
+const sizeButton = document.querySelector('.size')
 
 for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
     div.classList.add('square');
     container.appendChild(div);
 }
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach((sqaure) => {
+    sqaure.addEventListener('mouseover', (e) => {
+        console.log("gone over!");
+        e.target.classList.add('marker');
+    })
+})
+
+
+resetButton.addEventListener('click', () => location.reload());
